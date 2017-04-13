8552f053f96efb7f4c44b475f6525c47430e22e1
Kodlee Yin <kodlee@fru1t.me>
2017-04-13 15:44:55 -0700

    Removed unused import; Added better javadoc to methods and class

--- src/main/java/com/jonathanpli/exceptionnull/controller/IndexController.java
+++ src/main/java/com/jonathanpli/exceptionnull/controller/IndexController.java
@@ -1,21 +1,22 @@
 package com.jonathanpli.exceptionnull.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- *
+ * Routes root and error pages to their respective URLs. Also contains the fallback case.
  */
 @Controller
 @RequestMapping(value = "/")
 public class IndexController {
-	@Autowired
-	Environment env;
 
-	@RequestMapping
+	/**
+	 * Fallback and index page routing.
+	 * @return
+	 */
+	@RequestMapping(value = "*", method = { RequestMethod.GET, RequestMethod.POST } )
 	public ModelAndView getIndex() {
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("example", "This is in the and  aaaaa");
